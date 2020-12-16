7d04b511bb3966c15c240fb52063a7d0facb19b3
Putri ayuni <56281908+putriayuni91@users.noreply.github.com>
2020-12-16 23:19:46 +0700

    Add files via upload

---
+++ application/views/home/index.php
@@ -0,0 +1,39 @@
+<h1>LAPOR!</h1>
+<form action="" method="post">
+<div class="search">
+<input type="text" name="keyword" id="keyword" placeholder="cari..." >
+<button type="submit">Submit</button>
+</form>
+</div>
+<?php if($this->session->flashdata('flash')):?>
+<div>Data <strong>berhasil</strong> ditambahkan!<?= $this->session->flashdata('flash');?>
+</div>
+<?php endif;?>
+<br>
+<div class="buat">
+<a href="<?=base_url()?>data/tambah">Buat laporan</a>
+</div>
+
+<div class="tulisan">
+<p>laporan/komentar</p>
+</div>
+
+<div class="tulisan2">
+<div class="newshead">
+<ul class="detail">
+<?php foreach($lapor as $dt):?>
+<?= $dt['lapor'].' ';?>
+<br>
+<?= $dt['aspek'].' ';?>
+<?= $dt['gambar'].' ';?>
+<?= $dt['waktu'].' ';?>
+<li class="detail">
+	
+	<a href="<?=base_url()?>data/detail/<?=$dt['id']; ?>">detail..</a>
+
+</li>
+<br>
+<?php endforeach?>
+</ul>
+</div>
+</div>
